9cb0a4424cd1aa8f525c15fd715010108f071b10
Ananth S Pai <an20350028@wipro.com>
2023-02-20 17:07:10 +0530

    Asignment 2 first commit

---
+++ PythonAsign2.py
@@ -0,0 +1,21 @@
+StrVowel=input("Enter the string to get the count: ")
+vowels='aeiou'
+x=0
+str1=""
+n1=""
+for i in StrVowel:
+    if i==vowels[x]:
+        str1=str1+i
+    if x<(len(vowels)-1):
+        if i==vowels[x+1]:
+            str1=str1+i
+            x=x+1
+for i in StrVowel:
+    if i not in n1:
+        n1=n1+i
+
+if n1!=vowels:
+    print(0)
+else:
+    print(len(str1))
+            
